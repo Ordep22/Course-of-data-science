f1f21e284d2c27d2793408f54f932a9a6e22858f
Pedro Vitor Pereira <pedro.pereira@lupatecnologia.com.br>
2023-06-12 15:11:10 -0300

    Numpy Arrays

---
+++ Cap09 - Mathematics and Matrix Manipulation with Numpy/Numpy Arrays.py
@@ -0,0 +1,18 @@
+#Importing the packeg with we´ll work
+
+import numpy as np
+
+#Criating an Array with packege Numpy
+
+
+arrOne  = np.array([10,21,32,43,48,15,76,57,89])
+
+#show array One
+
+print(f"The first array is: {arrOne}")
+
+print(f"The tipe of first array is: {type(arrOne)}")
+
+print(f"The shape of first array is: {arrOne.shape}")
+
+
