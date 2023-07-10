29a8ce402068e72e12fb41941e3ba9bef7650f33
Pedro Vitor Pereira <pedro.pereira@lupatecnologia.com.br>
2023-07-10 14:53:29 -0300

    Finish manipulating files with Matplotlib

---
+++ Cap09 - Mathematics and Matrix Manipulation with Numpy/Statistical analysis with numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+#Criating an array with numpy
+
+arrayOne  =  np.array([15,20,18,12,2,2])
+
+#Calculating the average of this array
+
+
+print(f"The average of arraOne is: {arrayOne.mean()}\n")
+
+
