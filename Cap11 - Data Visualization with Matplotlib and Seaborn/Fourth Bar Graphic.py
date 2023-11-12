4a933d7f24f151988fde0515380987d544fd8dac
unknown <pedrovitorcomercial@gmail.com>
2023-11-12 20:09:44 -0300

    Finish Fifth Bar Graphic

--- Cap11 - Data Visualization with Matplotlib and Seaborn/Fourth Bar Graphic.py
+++ Cap11 - Data Visualization with Matplotlib and Seaborn/Fourth Bar Graphic.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 ages  = [22,65,45,21,22,34,42,4,99,101,120,122]
 bins  = [0,10,20,30,40,50,60,70,80,90,100,110,120,130]
 
+plt.title('Rwidth iquals 0.5')
 plt.hist(ages, bins,histtype = 'bar', rwidth = 0.5)
 plt.show()
 
+plt.title('Rwidth iquals 0.8')
+plt.hist(ages, bins,histtype = 'bar', rwidth = 0.8)
+plt.show()
 
 
