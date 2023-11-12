f749aded9f14d3754147e6c5bd06ddfb8cf9269f
unknown <pedrovitorcomercial@gmail.com>
2023-11-12 19:02:03 -0300

    Finish Third graphic

---
+++ Cap11 - Data Visualization with Matplotlib and Seaborn/Third Plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+listOne = [1,2,3]
+listTow = [14,12,10.5]
+
+plt.plot(listOne,listTow,label = 'Line One')
+#plt.xlabel('Place')
+#plt.ylabel('Average')
+plt.legend()
+plt.title('Third Grafic')
+plt.show()
