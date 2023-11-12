d89cbacc1a244c7c9062ad29b6785364e663a699
unknown <pedrovitorcomercial@gmail.com>
2023-11-12 19:23:58 -0300

    Finish First Bar Graphics

---
+++ Cap11 - Data Visualization with Matplotlib and Seaborn/First Bar Graphics.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+listOne  = [2,4,6,8,10]
+listTow = [6,7,8,2,4]
+
+#Criating the bar graphics
+plt.bar(listOne,listOne,label = 'Bar',color = 'grey')
+plt.legend()
+plt.show()
+
