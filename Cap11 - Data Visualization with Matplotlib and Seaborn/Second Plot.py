be76339ff891b5c92eaa47b4018dfb11a2a6e41e
unknown <pedrovitorcomercial@gmail.com>
2023-11-12 18:54:00 -0300

    Finish Second plot

---
+++ Cap11 - Data Visualization with Matplotlib and Seaborn/Second Plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+#Criating tow lists
+listOne = [2,3,5]
+listTow = [3,5,7]
+
+plt.plot(listOne,listTow)
+plt.xlabel('listOne')
+plt.ylabel('listTow')
+plt.title('Second Plot with Matplotlib')
+plt.show()
