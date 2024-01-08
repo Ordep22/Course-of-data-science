3fd699449adfa83dab2d22aefb1f3383b866cd21
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2024-01-08 20:25:07 -0300

    Starting line graphic with Pylab

---
+++ Cap11 - Data Visualization with Matplotlib and Seaborn/Sixth - Graphic Customize with Pylab.py
@@ -0,0 +1,16 @@
+from pylab import*
+"""
+[EN]
+
+
+[PT-BR]
+Pylab é um módulo fornceido pela biblioteca Matplotlib que combina a funcionalidade do pacote
+Numpy com a funcionalidade do pacote Pyplot. Ele fornece um ambiente de platagem interativo, permitingo que 
+os usuários criem rapidamente gráficos e visualizações de dados.
+Mo módulo Pylab inclui muitas funções úteis para plotagem de gráficos, como funções para criar gráficos
+de dispercão, barras, pizza, hisotgramas e muito mais. Ele tamém fornece para personalizar as configurações de plotagem,
+como titulos de eixo, rótulos de eixo, titulo de gráficos e cores. 
+
+"""
+
+
