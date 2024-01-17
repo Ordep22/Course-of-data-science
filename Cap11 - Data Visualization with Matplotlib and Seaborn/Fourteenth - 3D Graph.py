a02d7a986e5dce3b70ca33ec9f3665922807f26a
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2024-01-16 22:47:25 -0300

    Finish Fourteenth - 3D Graph

--- Cap11 - Data Visualization with Matplotlib and Seaborn/Fourteenth - 3D Graph.py
+++ Cap11 - Data Visualization with Matplotlib and Seaborn/Fourteenth - 3D Graph.py
@@ -16,7 +16,22 @@ phi_p = np.linspace(0, 2*np.pi,100)
 x,y = np.meshgrid(phi_p,phi_m)
 z = ColorMap(x,y).T
 
-fig = plt.figure(figsize=(14,6))
+fig = plt.figure(figsize=(16,6))
+#first graph
+ax = fig.add_subplot(1,3,1, projection = '3d')
+#p = ax.plot_surface(x,y,z,rstride=4,cstride=4, linewidth = 0)
+p = ax.plot_surface(x,y,z,rcount = 50,ccount = 50, linewidth = 0)
+
+#Second Graph
+ax = fig.add_subplot(1,3,2, projection = '3d')
+p = ax.plot_surface(x,y,z,rstride=1,cstride=1,linewidth = 0,cmap = cm.coolwarm, antialiased = False)
+
+#Threeth Graph
+ax = fig.add_subplot(1,3,3, projection  = '3d')
+p = ax.plot_wireframe(x,y,z,rstride=5,cstride=5)
+
+cb = fig.colorbar(p,shrink = 0.5)
+
 plt.show()
 
 
