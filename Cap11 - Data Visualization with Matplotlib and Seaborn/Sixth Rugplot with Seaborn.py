c703d99121bc2838ae867e96f52b50643e4d61d5
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2024-01-21 21:25:16 -0300

    Sixth Rugplot with Seaborn

--- Cap11 - Data Visualization with Matplotlib and Seaborn/Sixth Rugplot with Seaborn.py
+++ Cap11 - Data Visualization with Matplotlib and Seaborn/Sixth Rugplot with Seaborn.py
@@ -1,11 +1,3 @@
-'''
-DEPRECATED
-
-This function has been deprecated and will be removed in seaborn v0.14.0.
-It has been replaced by histplot() and displot(), two functions with a modern
- API and many more capabilities.
-'''
-
 import seaborn as sea
 import random
 import pandas as pd
@@ -16,7 +8,9 @@ dataSet  = pd.DataFrame()
 dataSet['Age'] = random.sample(range(20,100),30)
 dataSet['Weigth'] = random.sample(range(45,180),30)
 
-sea.distplot(dataSet.Weigth)
+
+plt.hist( dataSet.Age,alpha =.3)
+sea.rugplot(dataSet.Age)
 plt.show()
 
 
