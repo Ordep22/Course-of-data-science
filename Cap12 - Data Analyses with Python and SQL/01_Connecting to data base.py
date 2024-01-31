8c09d76b136b93d88f43f35514970a82cdfe9b1a
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2024-01-31 20:24:38 -0300

    Lesson Analyzing the data base with SQL and Python finished

--- Cap12 - Data Analyses with Python and SQL/01_Connecting to data base.py
+++ Cap12 - Data Analyses with Python and SQL/01_Connecting to data base.py
@@ -36,13 +36,74 @@ try:
     print(colunmsName)
     print("\n")
 
-    #Show the informations in the data base
+    #Show the informations in the database
     data  = cursor.fetchall()
 
     print("Database")
     for i in data:
         print(i)
     print("\n")
+
+    #Creating the Query to calculate the average of sales unite
+    salesQuery  = 'SELECT AVG(Unidades_Vendidas) FROM tb_vendas_dsa'
+
+    #Execute the query on database
+    resultAvg = cursor.execute(salesQuery)
+
+    print("The average of sales unite is:")
+    print(resultAvg.fetchall())
+    print("\n")
+
+
+    #Creating the Query that it'll select the average from the sales products
+    productsAVGQuery = 'SELECT Nome_Produto, AVG(Unidades_Vendidas) FROM tb_vendas_dsa GROUP BY Nome_Produto'
+
+    #Execute the last query
+    result_productsAVGQuery = cursor.execute(productsAVGQuery)
+
+    #Show the result of the execution
+    print("The avarage from the sales product is:")
+
+    for item in result_productsAVGQuery.fetchall():
+        print(item)
+    print("\n")
+
+
+    #Creating the Query that it'll filter the product sales value whem it was biger them 199
+
+    filterQuerry  = 'SELECT Nome_Produto, AVG(Unidades_Vendidas) FROM tb_vendas_dsa WHERE Valor_Unitario > 199 GROUP BY Nome_Produto'
+
+    resultFilter = cursor.execute(filterQuerry)
+
+    for item in resultFilter.fetchall():
+
+        print(item)
+
+    print("\n")
+
+
+
+    filterQuerry_2 = 'SELECT Nome_Produto, AVG(Unidades_Vendidas) FROM tb_vendas_dsa WHERE Valor_Unitario > 199 GROUP BY Nome_Produto HAVING AVG(Unidades_Vendidas) > 10 '
+
+    resultFilter_2 = cursor.execute(filterQuerry_2)
+
+    for item in resultFilter_2:
+        print(item)
+
+    print('\n')
+
+
+
+
+
+
+
+
+    cursor.close()
+    dataBase.close()
+
+
+
 
 
 
@@ -53,4 +114,5 @@ try:
 
 
 except:
+
     print("Something went wrong!")
