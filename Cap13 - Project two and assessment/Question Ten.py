64c0dd61e79b29259f6e0a1006c971cfdebac2a4
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2024-03-07 22:06:25 -0300

    Start lesson ten

---
+++ Cap13 - Project two and assessment/Question Ten.py
@@ -0,0 +1,11 @@
+from libraries import *
+
+data = pd.read_csv("/Users/PedroVitorPereira/Documents/GitHub/Course-of-data-science/Cap13 - Project two and assessment/dados/dataset.csv")
+
+dataSet  = pd.DataFrame(data)
+
+best_category_sales  = pd.grou
+
+
+
+
