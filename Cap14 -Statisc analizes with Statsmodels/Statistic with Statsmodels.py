d1cee48403b2fc36ec0737d419ddffa40e75e350
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2024-03-23 00:09:15 -0300

    Cpmpliting the exploration analysis

--- Cap14 -Statisc analizes with Statsmodels/Statistic with Statsmodels.py
+++ Cap14 -Statisc analizes with Statsmodels/Statistic with Statsmodels.py
@@ -26,6 +26,29 @@ print(30*"--")
 print("Statistic resume")
 print(dataFrame.describe())
 
+#Rusume of target varible
+print(30*"--")
+print("Resume of target variable")
+print(dataFrame['valor_aluguel'].describe())
+
+#Show the informations in a graph
+sea.histplot(data = dataFrame, x = 'valor_aluguel',kde= True)
+plt.savefig("/Users/PedroVitorPereira/Documents/GitHub/Course-of-data-science/Cap14 -Statisc analizes with Statsmodels/Hist_valor_aluguel.png")
+plt.close()
+
+#Correlation matrix
+print(30*"--")
+print("Correlation Matrix")
+print(dataFrame.corr())
+
+sea.scatterplot(data = dataFrame, x = "area_m2", y = "valor_aluguel")
+plt.savefig("/Users/PedroVitorPereira/Documents/GitHub/Course-of-data-science/Cap14 -Statisc analizes with Statsmodels/Correlation_Graph.png")
+plt.close()
+
+
+
+
+
 
 
 
